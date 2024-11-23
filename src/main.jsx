c6e6696c09d68185bd0ee868d87b4d73154b264a
Leo <leo96pereira@hotmail.com>
2024-11-23 13:17:58 +0000

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "@material-tailwind/react";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
