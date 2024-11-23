e46af03d7725f400ff9a2969b86302553fd6697d
chari00 <macharitocruzuk@gmail.com>
2024-11-23 11:50:05 +0000

    add componenst folder and create readme file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { Button, Card, CardBody, CardFooter, Typography } from "@material-tailwind/react";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <Card className="w-96">
-        <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            Welcome to Material Tailwind
-          </Typography>
-          <Typography>
-            This is a simple example of using Material Tailwind components in a Vite React project.
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Button>Read More</Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <>
+    <div> <h1>hello world</h1>   </div>
+    
+    </>
   )
 }
 
